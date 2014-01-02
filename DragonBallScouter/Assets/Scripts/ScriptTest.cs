fdf4e1e6199a82aa91287808605044d37bc55815
Pablo Pera <pablo.sx@gmail.com>
2014-01-02 11:50:41 -0500

    Change smiley type

--- DragonBallScouter/Assets/Scripts/ScriptTest.cs
+++ DragonBallScouter/Assets/Scripts/ScriptTest.cs
@@ -6,7 +6,7 @@ public class ScriptTest : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 	
-		 print("Hello :)");
+		 print("Hello :D");
 	}
 	
 	// Update is called once per frame
