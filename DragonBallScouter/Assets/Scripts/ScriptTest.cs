2c6ed9b4cfe0455f938a7a6f88e70368f63d0ed4
JorgeOrtaLópez <caxiulo@gmail.com>
2014-01-02 17:48:55 +0100

    Add Script

---
+++ DragonBallScouter/Assets/Scripts/ScriptTest.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScriptTest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+		 print("Hello :)");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
